3930a88af7645a1375d0d28d90be835ee97be5ba
kamten07 <1422180499@qq.com>
2026-09-07 00:28:23 +0800

    补全数据层，新增document实体类中的summary字段，返回文件的风险摘要，新增风险结果和风险详情表的数据层

---
+++ anxin-web/src/main/java/com/anxin/mapper/DocumentSectionMapper.java
@@ -0,0 +1,9 @@
+package com.anxin.mapper;
+
+import com.anxin.entity.DocumentSection;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DocumentSectionMapper extends BaseMapper<DocumentSection> {
+}
---
+++ anxin-web/src/main/java/com/anxin/mapper/RiskResultMapper.java
@@ -0,0 +1,9 @@
+package com.anxin.mapper;
+
+import com.anxin.entity.RiskResult;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RiskResultMapper extends BaseMapper<RiskResult> {
+}
